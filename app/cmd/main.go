95b30b8c8927cfb2a09ba35cf308efda614a1829
gamepkw <game9074@gmail.com>
2024-02-06 13:07:15 +0700

    commit

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/labstack/echo"
+	"github.com/spf13/viper"
+
+	_handler "github.com/gamepkw/test-argo/app/internal/handler"
+)
+
+func init() {
+	viper.SetConfigFile(`../../config/config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func main() {
+	e := echo.New()
+	_handler.NewHandler(e)
+
+	log.Fatal(e.Start(viper.GetString("server.address")))
 }
--- app/internal/handler/handler.go
+++ app/internal/handler/handler.go
@@ -1,1 +1,26 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type handler struct {
+}
+
+func NewHandler(e *echo.Echo) {
+	handler := &handler{}
+	e.GET("/test", handler.Test)
+}
+
+type Handler interface {
+	Test(c echo.Context) error
+}
+
+func (h *handler) Test(c echo.Context) error {
+
+	response := "success"
+
+	return c.JSON(http.StatusOK, response)
+}
